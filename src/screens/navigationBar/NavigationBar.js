7d6f9935089bc24828ad108f292c6634b9f0d747
kkm980 <kantkrishna751@gmail.com>
2021-10-24 22:59:02 +0530

    v2.2

--- src/screens/navigationBar/NavigationBar.js
+++ src/screens/navigationBar/NavigationBar.js
@@ -4,7 +4,7 @@ import './navigation.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { navigationLinks } from '../../helpers/navigationLinks'
-
+import siglog from "../../helpers/img/siglog.gif"
 import Aos from "aos"
 import "aos/dist/aos.css"
 
@@ -21,15 +21,13 @@ function NavigationBar() {
     Aos.init({duration:1000});
   }, [])
 
-
-  
   return (
     <div id="home">
       <Navbar className="navigation__container" style={{
         zIndex: '2', position: 'fixed', top: '0', width: '100%'
       }} collapseOnSelect
         expand="md">
-        <Navbar.Brand data-aos="slide-right" style={{ marginLeft: '1rem' }} href="#home">Krishna Kant</Navbar.Brand>
+        <img  data-aos="slide-right" style={{height:"35px", marginLeft: '1rem' }} href="#home" src={siglog} alt=""/>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse style={{ justifyContent: 'flex-end', marginRight: '1rem', borderColor: 'none' }}>
           <Nav data-aos="slide-right" className="links" style={{ margin: '0 1rem' }}>
